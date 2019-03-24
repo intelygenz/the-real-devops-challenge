a95c5c91307bfeb5ba3acfcbba36e77f01b9a177
dalonsogz <dalonsogz@yahoo.es>
2019-03-24 18:56:47 +0100

    Test phase fix try 2

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ echo "***************************"
 echo "** Testing Docker Image ***"
 echo "***************************"
 
-docker run -it -v $(pwd):/tmp/app -w /tmp/app --rm painless/tox /bin/bash tox
+docker run -t -v $(pwd):/tmp/app -w /tmp/app --rm painless/tox /bin/bash tox
