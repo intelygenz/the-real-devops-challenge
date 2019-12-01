39059efdfa87daa290f66ed1bd893a11dca9f599
Rosa Carpio <rosa.carpio.lopez@cgi.com>
2019-12-01 23:03:23 +0100

    Solution Challenge 1: - app.py: Restaurant function updated to return a json objectif there is a match and a http 204 status code if there is not a match. - mongoflask.py: find_restaurants funcion updated to fix the bug

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from os import environ
 
-from bson import json_util
+import bson
+from bson.json_util import loads
 from bson.objectid import ObjectId
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 
 from src.mongoflask  import MongoJSONEncoder, ObjectIdConverter, find_restaurants
+
+if not environ.get("MONGO_URI"):
+    raise ValueError("No MONGO_URI set for Flask application")
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = environ.get("MONGO_URI")
@@ -13,6 +17,12 @@ app.json_encoder = MongoJSONEncoder
 app.url_map.converters["objectid"] = ObjectIdConverter
 mongo = PyMongo(app)
 
+@app.route("/api/v1/restaurant/status")
+def status():
+    return jsonify(
+        status=200,
+        message='Restaurants app lives!'
+    )
 
 @app.route("/api/v1/restaurant")
 def restaurants():
@@ -23,7 +33,13 @@ def restaurants():
 @app.route("/api/v1/restaurant/<id>")
 def restaurant(id):
     restaurants = find_restaurants(mongo, id)
-    return jsonify(restaurants)
+    if len(restaurants) != 1:
+        response = "Error finding the restaurant"
+        code = 204        
+    else:
+        response = restaurants[0]
+        code = 200
+    return jsonify(response), code    
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=False, port=8080)
+    app.run(host="0.0.0.0", debug=True, port=8080)
