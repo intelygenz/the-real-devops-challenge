b2263acf97417ab0f992438e5e9c27458d837240
Rosa Carpio <rosa.carpio.lopez@cgi.com>
2019-12-01 23:13:43 +0100

    Solution Challenge 5: - .gitignore modified to ignore passwords files - init-mongo.sh added. It is a script for creating a user and importing the dataset to MongoDB at start up. - docker-compose.yml file added.

---
+++ config/init-mongo.sh
@@ -0,0 +1,12 @@
+mongo -- "$MONGO_INITDB_DATABASE" <<EOF
+    var rootUser = '$MONGO_INITDB_ROOT_USERNAME';
+    var rootPassword = '$(cat "$MONGO_INITDB_ROOT_PASSWORD_FILE")';
+    var admin = db.getSiblingDB('admin');
+    admin.auth(rootUser, rootPassword);
+
+    var user = '$MONGO_INITDB_USERNAME';
+    var passwd = '$(cat "$MONGO_INITDB_PASSWORD_FILE")';
+    db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
+EOF
+
+mongoimport -u $MONGO_INITDB_USERNAME -p $(cat "$MONGO_INITDB_PASSWORD_FILE") --db $MONGO_INITDB_DATABASE --collection restaurant --type json --file /config/restaurant.json
