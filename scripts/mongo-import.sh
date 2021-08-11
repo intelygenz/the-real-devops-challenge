bd4b73b3b8a63ac07395635a43b7e1a20ac27eaf
Borja Lopez <borja.lopez.pinan@gmail.com>
2021-08-11 21:33:59 +0200

    fix

--- scripts/mongo-import.sh
+++ scripts/mongo-import.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e;
 
-mongoimport --host "${MONGO_HOST}" --port 27017 --username "${MONGO_USERNAME}" --password "${MONGO_PASSWORD}" --db "${MONGO_DATABASE}" --collection "${MONGO_DATABASE}" --authenticationDatabase admin --drop --file /docker-entrypoint-initdb.d/restaurant.json 
+mongoimport --host "${MONGO_HOST}" --port 27017 --username "${MONGO_USERNAME}" --password "${MONGO_PASSWORD}" --db "${MONGO_DATABASE}" --collection "${MONGO_DATABASE}" --authenticationDatabase admin --authenticationMechanism=SCRAM-SHA-256 --drop --file /docker-entrypoint-initdb.d/restaurant.json 
