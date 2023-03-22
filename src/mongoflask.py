2325e75a97730866436067f06f3e456d11e7922d
justor7 <128474121+justor7@users.noreply.github.com>
2023-03-22 12:04:34 +0100

    _id bug && jsonify(list()) ---> have to change list to first element on final result

--- src/mongoflask.py
+++ src/mongoflask.py
@@ -26,7 +26,7 @@ class ObjectIdConverter(BaseConverter):
 def find_restaurants(mongo, _id=None):
     query = {}
     if _id:
-        query["_id"] = ObjectId(id)
+        query["_id"] = ObjectId(_id)
     result = mongo.db.restaurant.find(query)
     if _id == None:
         result = list(result)        
